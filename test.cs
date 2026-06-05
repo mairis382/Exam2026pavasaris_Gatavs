d810cbc9fb9e87cdb34123365f61a79dfce32137
mairis382 <mairis382@gmail.com>
2026-06-05 21:50:07 +0300

    Add files via upload

--- test.cs
+++ test.cs
@@ -1,7 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
-<packages>
-  <package id="MSTest.TestAdapter" version="2.2.10" targetFramework="net472" />
-  <package id="MSTest.TestFramework" version="2.2.10" targetFramework="net472" />
-  <package id="Selenium.WebDriver" version="4.20.0" targetFramework="net472" />
-  <package id="Selenium.WebDriver.ChromeDriver" version="124.0.6367.6000" targetFramework="net472" />
-</packages>
+<Project ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <Import Project="$(MSBuildToolsPath)\Microsoft.CSharp.targets" />
+  <PropertyGroup>
+    <Configuration Condition=" '$(Configuration)' == '' ">Debug</Configuration>
+    <Platform Condition=" '$(Platform)' == '' ">AnyCPU</Platform>
+    <ProjectGuid>{7DF25F06-D9EF-4F43-B159-9DDF3D4E9E2A}</ProjectGuid>
+    <OutputType>Library</OutputType>
+    <RootNamespace>ClassLibrary1</RootNamespace>
+    <AssemblyName>ClassLibrary1</AssemblyName>
+    <TargetFrameworkVersion>v4.7.2</TargetFrameworkVersion>
+  </PropertyGroup>
+  <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' ">
+    <DebugSymbols>true</DebugSymbols>
+    <DebugType>full</DebugType>
+    <Optimize>false</Optimize>
+    <OutputPath>bin\Debug\</OutputPath>
+    <DefineConstants>DEBUG;TRACE</DefineConstants>
+    <WarningLevel>4</WarningLevel>
+  </PropertyGroup>
+  <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' ">
+    <DebugType>pdbonly</DebugType>
+    <Optimize>true</Optimize>
+    <OutputPath>bin\Release\</OutputPath>
+    <DefineConstants>TRACE</DefineConstants>
+    <WarningLevel>4</WarningLevel>
+  </PropertyGroup>
+  <ItemGroup>
+    <Reference Include="System" />
+    <Reference Include="System.Core" />
+    <Reference Include="Microsoft.VisualStudio.TestPlatform.TestFramework" />
+  </ItemGroup>
+  <ItemGroup>
+    <Compile Include="Tests\test.cs" />
+    <None Include="packages.config" />
+  </ItemGroup>
+</Project>
